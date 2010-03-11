3c7529c22b7ae4ea9ae2c6eccf1b1683cae389a1
Jeremy VanDerWal <jjvanderwal@gmail.com>
2010-03-11 04:24:02 +0000

    simplified extract.data.R

--- R/extract.data.R
+++ R/extract.data.R
@@ -9,15 +9,3 @@ function(pts, x) {
   yf <- as.numeric(cut(pts[, 2], xy$y))
   return(x[cbind(xf,yf)])
 }
-#
-#function(pts, x) {
-#  xy <- getXYcoords(x)
-#  xy$x <- xy$x + attr(x, "cellsize")/2
-#  xy$x <- c(xy$x, xy$x[1] - attr(x, "cellsize")/2)
-#  xy$y <- xy$y + attr(x, "cellsize")/2
-#  xy$y <- c(xy$y, xy$y[1] - attr(x, "cellsize")/2)
-#  xf <- as.numeric(cut(pts[, 1], xy$x))
-#  yf <- as.numeric(cut(pts[, 2], xy$y))
-#  return(x[cbind(xf,yf)])
-#}
-#
