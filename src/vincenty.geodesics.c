81bafdf15b2499603e314ff88d4decec1fe01ed0
Jeremy VanDerWal <jjvanderwal@gmail.com>
2010-07-17 04:12:39 +0000

    corrected distance code

--- src/vincenty.geodesics.c
+++ src/vincenty.geodesics.c
@@ -88,7 +88,7 @@ SEXP dist(SEXP latitude1, SEXP longitude1, SEXP latitude2, SEXP longitude2) {
 	PROTECT(latitude2 = coerceVector(latitude2, REALSXP)); double *lat2 = REAL(latitude2);
 	PROTECT(longitude2 = coerceVector(longitude2, REALSXP)); double *lon2 = REAL(longitude2);
 	
-	int npnts = length(lat1); //get the number of points
+	int npnts = length(latitude1); //get the number of points
 
 	//setup the output vector and allocate everything as NA to begin with
 	double *out; SEXP ans;
@@ -96,6 +96,7 @@ SEXP dist(SEXP latitude1, SEXP longitude1, SEXP latitude2, SEXP longitude2) {
 	out = REAL(ans); //pointer to output dataset
 
 	//cycle through each of the pairings and return the lengths
+	int ii;
 	for (ii=0;ii<npnts;ii++) {
 		double L = (lon2[ii]-lon1[ii]) * (PI/180);
 		double U1 = atan((1-f) * tan(lat1[ii] * (PI/180)));
@@ -104,28 +105,30 @@ SEXP dist(SEXP latitude1, SEXP longitude1, SEXP latitude2, SEXP longitude2) {
 		double sinU2 = sin(U2), cosU2 = cos(U2);
 	  
 		double lambda = L, lambdaP, iterLimit = 100;
+		double sinLambda, cosLambda, sinSigma, cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM, C;
 		do {
-			double sinLambda = sin(lambda), cosLambda = cos(lambda);
-			double sinSigma = sqrt((cosU2*sinLambda) * (cosU2*sinLambda) + (cosU1*sinU2-sinU1*cosU2*cosLambda) * (cosU1*sinU2-sinU1*cosU2*cosLambda));
+			sinLambda = sin(lambda);
+			cosLambda = cos(lambda);
+			sinSigma = sqrt((cosU2*sinLambda) * (cosU2*sinLambda) + (cosU1*sinU2-sinU1*cosU2*cosLambda) * (cosU1*sinU2-sinU1*cosU2*cosLambda));
 			if (sinSigma==0) return 0;  // co-incident points
-			double cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda;
-			double sigma = atan2(sinSigma, cosSigma);
-			double sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
-			double cosSqAlpha = 1 - sinAlpha*sinAlpha;
-			double cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha;
+			cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda;
+			sigma = atan2(sinSigma, cosSigma);
+			sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+			cosSqAlpha = 1 - sinAlpha*sinAlpha;
+			cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha;
 			if (isnan(cos2SigmaM)) cos2SigmaM = 0;  // equatorial line: cosSqAlpha=0 (§6)
-			double C = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));
+			C = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));
 			lambdaP = lambda;
 			lambda = L + (1-C) * f * sinAlpha * (sigma + C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)));
-		} while (Math.abs(lambda-lambdaP) > 1e-12 && --iterLimit>0);
+		} while (abs(lambda-lambdaP) > 1e-12 && --iterLimit>0);
 
 		double uSq = cosSqAlpha * (a*a - b*b) / (b*b);
 		double A = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));
 		double B = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)));
 		double deltaSigma = B*sinSigma*(cos2SigmaM+B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)));
 		double s = b*A*(sigma-deltaSigma);
-		if (iterLimit==0) s=-9999  // formula failed to converge
-		ans[ii] = s
+		if (iterLimit==0) s=-9999;  // formula failed to converge
+		out[ii] = s;
 	}
 	UNPROTECT(5);
 	return(ans);
