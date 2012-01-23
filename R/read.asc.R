f5374731c268f2a0a37275cefd40a004787e74d9
Jeremy VanDerWal <jjvanderwal@gmail.com>
2012-01-23 00:16:01 +0000

    corrected issues with read.asc.gz and seek from gzfile

--- R/read.asc.R
+++ R/read.asc.R
@@ -6,33 +6,24 @@ function (file, gz=FALSE) {
 	} else {
 		if (substr(file, nchar(file) - 3, nchar(file)) != ".asc") stop("not a valid .asc file")
 	}
+	
 	#read in the header
 	if (gz) { zz <- gzfile(file, "r") } else { zz <- file(file, "r") }
-		#number of columns
-		nc <- readLines(zz, 1); nc <- strsplit(nc, " "); nc <- as.numeric(nc[[1]][length(nc[[1]])])
-		#number of rows
-		nl <- readLines(zz, 1); nl <- strsplit(nl, " "); nl <- as.numeric(nl[[1]][length(nl[[1]])])
-		#lower left corner
-		xll <- readLines(zz, 1); xll <- strsplit(xll, " ")
-		yll <- readLines(zz, 1); yll <- strsplit(yll, " ")
-		#cell size
-		cs <- readLines(zz, 1); cs <- strsplit(cs, " "); cs <- as.numeric(cs[[1]][length(cs[[1]])])
-		#nodata value
-		nas <- readLines(zz, 1); nas <- strsplit(nas, " "); nas <- as.numeric(nas[[1]][length(nas[[1]])])
+		nc <- scan(zz,what=list('',''),nlines=1); nc <- as.numeric(nc[[2]][1])#number of columns
+		nl <- scan(zz,what=list('',''),nlines=1); nl <- as.numeric(nl[[2]][1])#number of rows
+		xll <- scan(zz,what=list('',''),nlines=1); #lower left corner
+		yll <- scan(zz,what=list('',''),nlines=1); #lower left corner
+		cs <- scan(zz,what=list('',''),nlines=1); cs <- as.numeric(cs[[2]][1])#cell size
+		nas <- scan(zz,what=list('',''),nlines=1); nas <- as.numeric(nas[[2]][1])#nodata value
 	#close the link to the file
 	close(zz)
 	
 	#ensure xll & yll are centers of the cells
-	if ((xll[[1]][1] == "xllcenter") | (xll[[1]][1] == "XLLCENTER")) { xll=as.numeric(xll[[1]][length(xll[[1]])]) } else { xll=as.numeric(xll[[1]][length(xll[[1]])])+ cs/2 }
-	if ((yll[[1]][1] == "yllcenter") | (xll[[1]][1] == "YLLCENTER")) { yll=as.numeric(yll[[1]][length(yll[[1]])]) } else { yll=as.numeric(yll[[1]][length(yll[[1]])])+ cs/2 }
-	
+	if ((xll[[1]][1] == "xllcenter") | (xll[[1]][1] == "XLLCENTER")) { xll=as.numeric(xll[[2]][1]) } else { xll=as.numeric(xll[[2]][1])+ cs/2 }
+	if ((yll[[1]][1] == "yllcenter") | (xll[[1]][1] == "YLLCENTER")) { yll=as.numeric(yll[[2]][1]) } else { yll=as.numeric(yll[[2]][1])+ cs/2 }
 
 	#read in the data skipping the first six header rows
-	if (gz) { 
-		zz = gzfile(file, "r")
-			output <- scan(zz,nmax=nl*nc,skip=6,quiet = TRUE)
-			#output <- scan(zz,skip=6,nlines=nl,quiet = TRUE)
-		close(zz)
+	if (gz) { output <- scan(gzfile(file, "r"),nmax=nl*nc,skip=6,quiet = TRUE)
 	} else { output <- scan(file,nmax=nl*nc,skip=6, quiet = TRUE) }
 
 	#convert no data to NA
