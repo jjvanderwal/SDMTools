f422662cfbbcbd2fd4a44174c93b443eccc10f61
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-07-08 01:03:36 +0000

    updated Istat

--- R/Istat.R
+++ R/Istat.R
@@ -1,6 +1,6 @@
 #' I Similarity Statistic for Quantifying Niche Overlap
 #' 
-#' \code{Istat} computes the I similarity statistic of Warren et al. 2009. It
+#' \code{Istat} computes the I similarity statistic of Warren et al. 2008. It
 #' is a method for defining niche overlap from predictions of species'
 #' distributions. \cr \cr \bold{NOTE:} it is assumed the input data are of the
 #' same extent and cellsize, and all values are positive.
@@ -10,15 +10,21 @@
 #' distributions. The I similarity statistic ranges from a value of 0, where
 #' two distributions have no overlap, to 1 where two distributions are
 #' identical (Warren et al., 2008).
+#'
+#' NOTE: updated to correct equation but not to worry about old... see explanation at
+#' \url{http://enmtools.blogspot.com.au/2010_09_01_archive.html}.
 #' 
 #' @param x a vector or matrix of data; the matrix can be a raster of class
 #' 'asc' (adehabitat package), 'RasterLayer' (raster package) or
 #' 'SpatialGridDataFrame' (sp package)
 #' @param y a vector or matrix of data with the same dimensions and class of
 #' 'x'
+#' @param old a boolean identifying if "old" equation is to be used (see description).
+#' This was kept for legacy issues.
+#'
 #' @return A single value that is the I similarity statistic
 #' @author Jeremy VanDerWal \email{jjvanderwal@@gmail.com}
-#' @references Warren, D. L., R. E. Glor, M. Turelli, and D. Funk. 2009.
+#' @references Warren, D. L., R. E. Glor, M. Turelli, and D. Funk. 2008.
 #' Environmental Niche Equivalency versus Conservatism: Quantitative Approaches
 #' to Niche Evolution. Evolution 62:2868-2883.
 #' @examples
@@ -45,7 +51,7 @@
 #' 
 #' 
 #' @export 
-Istat = function(x,y){
+Istat = function(x,y,old=FALSE){
 	#check if raster from sp or raster package and convert if necessary
 	if (any(class(x) %in% 'RasterLayer')) { x = asc.from.raster(x); y = asc.from.raster(y) }
 	if (any(class(x) == 'SpatialGridDataFrame')) { x = asc.from.sp(x); y = asc.from.sp(y) }
@@ -54,6 +60,10 @@ Istat = function(x,y){
 	pos = which(is.finite(x) & is.finite(y)) #positions in the data which have a value (are not NA)
 	px = x[pos]/sum(x[pos]) #calculate the proportionate value relative to the sum of values across the raster
 	py = y[pos]/sum(y[pos]) #calculate the proportionate value relative to the sum of values across the raster
-	I = 1 - 0.5 * sqrt(sum((sqrt(px)-sqrt(py))^2)) #calculate the I-Statistic
-	return(I)
+	H = sqrt(sum((sqrt(px)-sqrt(py))^2)) #define the Hellenger distance
+	if (old) {
+		return (1 - 0.5 * H) #return the old I-Statistic
+	} else {
+		return (1 - (H^2)/2) #return the corrected I-Statistic
+	}
 }
