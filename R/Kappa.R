9a23495c4dd99607ef7219a8603bd18a92076e16
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-07-08 10:28:43 +0000

    updated Kappa to avoid integer overflow

--- R/Kappa.R
+++ R/Kappa.R
@@ -31,8 +31,8 @@ Kappa <- function(mat){
 	if (attr(mat,'class')!='confusion.matrix') stop('mat must be of class confusion.matrix')
 	#calculate Kappa
 	n<-sum(mat)
-	colsums<-apply(mat,2,sum)
-	rowsums<-apply(mat,1,sum)
+	colsums<-as.double(apply(mat,2,sum))
+	rowsums<-as.double(apply(mat,1,sum))
 	t1 = sum(diag(mat))/n; 	t2 = sum(rowsums*colsums)/(n^2)
 	#return the value
 	return((t1-t2)/(1-t2))
